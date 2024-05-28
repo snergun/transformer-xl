1f013f6ebf78316b5804dcd45526be8b560a550b
snergun <161766738+snergun@users.noreply.github.com>
2024-05-28 12:42:01 -0700

    Update getwikidata.sh

--- getwikidata.sh
+++ getwikidata.sh
@@ -1,7 +1,11 @@
 "- Downloading wikitext-103-v1 (Word)"
 python prep_wiki.py
-cd wikitext-103
-    mv wiki.train.tokens train.txt
-    mv wiki.valid.tokens valid.txt
-    mv wiki.test.tokens test.txt
-    cd ..
+mkdir -p data
+mv wikitext-103/ data/wikitext-103/
+cd data/wikitext-103
+mv wiki.train.tokens train.txt
+mv wiki.valid.tokens valid.txt
+mv wiki.test.tokens test.txt
+cd ..
+cd ..
+rm wikitext-103.tar.gz
